ab12b6ed1c8a2a44a1d81d20a8d1b27fb80bf3fc
Herman Seternes <hermahs@stud.ntnu.no>
2025-02-05 15:21:04 +0100

    Create LLM module
    the LLM module takes in letters and constructs words, and guesses which word was input based on the letters. The guessing is done by FastText and a norwegian library

--- llm.py
+++ llm.py
@@ -1,8 +1,54 @@
-# LLM stuff
+import fasttext
+import fasttext.util
+import os
 
 
 class LLM:
-    def __init__(self) -> None:
+    letters: list[str]
+    max_letters: int
+    model: fasttext.FastText._FastText
+    norwegian_letters: set[str]
+
+    def __init__(
+        self,
+        max_letters: int = 5,
+        model_path: str = "model/cc.no.300.bin",
+        norwegian_path: str = "model/norwegian_words.txt",
+    ) -> None:
+        print("Starting LLM")
+        self.letters = []
+        self.max_letters = max_letters
+
+        if not os.path.isfile(model_path):
+            print("Downloading fasttext dataset")
+            fasttext.util.download_model("no", if_exists="ignore")
+            os.rename("cc.no.300.bin", model_path)
+            os.remove("cc.no.300.bin.gz")
+
+        if os.path.isfile(norwegian_path):
+            with open(norwegian_path, "r+") as f:
+                words = f.readlines()
+                words = [word.strip() for word in words]
+                self.norwegian_letters = set(words)
+        else:
+            self.norwegian_letters = set()
+        self.model = fasttext.load_model(model_path)
+
         pass
+
+    def get_result(self) -> str:
+        input_text = "".join(self.letters)
+        self.letters = []
+
+        if input_text in self.norwegian_letters:
+            return input_text
+
+        closest_words = self.model.get_nearest_neighbors(input_text, k=5)
+
+        return closest_words[0][-1]
+
+    def add_letter(self, letter: str) -> None:
+        self.letters.append(letter)
+        return
 
     pass
